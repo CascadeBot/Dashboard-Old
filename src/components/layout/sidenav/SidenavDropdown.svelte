465f614bd4394b466a3da3dfc654f9a7286b5ae1
Jelle van Snik <jellevs@gmail.com>
2020-07-28 16:56:58 +0200

    Sidenav server dropdown

--- src/components/layout/sidenav/SidenavDropdown.svelte
+++ src/components/layout/sidenav/SidenavDropdown.svelte
@@ -1,8 +1,16 @@
 <script>
+  import { fade, fly } from 'svelte/transition';
+  import { Link } from "svelte-routing";
+
   export let servers = [];
   export let skeleton = false;
   export let id;
 
+  /* dropdown */
+  let open = false;
+  let zindexShow = false;
+
+  /* server selection */
   let selected = {};
   let styleStr = "";
 
@@ -21,11 +29,38 @@
       <div class="img"></div>
     </div>
   {:else}
-    <div class="current">
-      <div class="img" style={styleStr}></div>
-      <div class="content">
-        <p class="name">{selected.Meta.name}</p>
-        <p class="count">{selected.Meta.memberCount} member{#if selected.Meta.memberCount > 1}s{/if}</p>
+    {#if open}
+      <div class="overlay"
+        on:click={()=>{open = false}}
+        on:outroend={()=>{zindexShow = false}}
+        on:introstart={()=>{zindexShow = true}}
+        transition:fade={{ duration: 100 }}></div>
+    {/if}
+    <div class="nav-dropdown" class:higher={zindexShow}>
+      <div class="current" on:click={()=>{open = !open}}>
+        <div class="img" style={styleStr}></div>
+        <div class="content">
+          <p class="name">{selected.Meta.name}</p>
+          <p class="count">{selected.Meta.memberCount} member{#if selected.Meta.memberCount > 1}s{/if}</p>
+        </div>
+        <div class="triangle" class:open={open}></div>
+      </div>
+      <div class="server-container">
+        {#if open}
+          <div class="servers" transition:fly={{ y: -10, duration: 100 }}>
+            {#each servers as server}
+              <Link to="/server/{server.id}" on:click={()=>{open = false}}>
+                <div class="server" style={server.id == id ? "display: none;" : ""}>
+                  <div class="img" style={server.Meta.iconURL ? `background-image: url('${server.Meta.iconURL}');` : ""}></div>
+                  <div class="content">
+                    <p class="name">{server.Meta.name}</p>
+                    <p class="count">{server.Meta.memberCount} member{#if server.Meta.memberCount > 1}s{/if}</p>
+                  </div>
+                </div>
+              </Link>
+            {/each}
+          </div>
+        {/if}
       </div>
     </div>
   {/if}
@@ -34,7 +69,60 @@
 <style lang="scss">
   @use "vars";
 
-  .current {
+  .nav-dropdown {
+    &.higher {
+      position: relative;
+      z-index: 51;
+    }
+
+    user-select: none;
+
+    .current {
+      position: relative;
+      z-index: 55;
+    }
+  }
+
+  .triangle {
+    width: 0;
+    height: 0;
+    border-left: 4px solid transparent;
+    border-right: 4px solid transparent;
+
+    border-top: 4px solid white;
+    transition: transform 100ms ease-in-out;
+    opacity: .8;
+
+    &.open {
+      transform: rotate(180deg);
+    }
+  }
+
+  .server-container {
+    display: block;
+    position: relative;
+  }
+
+  .servers {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+
+    margin-top: .3rem;
+    background-color: vars.$bg_300;
+    border-radius: 3px;
+    overflow: hidden;
+
+    > :global(a) {
+      text-decoration: none;
+      &:hover, &:active, &:focus, &:visited {
+        text-decoration: none;
+      }
+    }
+  }
+
+  .current, .server {
     background-color: vars.$bg_300;
     border-radius: 3px;
     display: flex;
@@ -82,5 +170,21 @@
         background-color: vars.$bg_600;
       }
     }
+  }
+
+  .server {
+    border-radius: 0;
+  }
+
+  .overlay {
+    background-color: black;
+    opacity: .75;
+
+    position: fixed;
+    z-index: 50;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
   }
 </style>
